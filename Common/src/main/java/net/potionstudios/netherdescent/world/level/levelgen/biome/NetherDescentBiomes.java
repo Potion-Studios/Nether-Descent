affab2e71b507f072f4d05b05fdba3c705f8e1b8
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-06-20 20:02:13 -0400

    add Sythian Torrids

--- Common/src/main/java/net/potionstudios/netherdescent/world/level/levelgen/biome/NetherDescentBiomes.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/level/levelgen/biome/NetherDescentBiomes.java
@@ -21,8 +21,9 @@ public class NetherDescentBiomes {
 	public static final Map<ResourceKey<Biome>, BiomeFactory> BIOME_FACTORIES = new Reference2ObjectOpenHashMap<>();
 	public static final Multimap<TagKey<Biome>, ResourceKey<Biome>> BIOMES_BY_TAG = Multimaps.newSetMultimap(new HashMap<>(), HashSet::new);
 
-	public static final ResourceKey<Biome> WAILING_GARTH = createBiome("wailing_garth", NetherDescentBiomeBuilder::wailingGarth);
 	public static final ResourceKey<Biome> EMBUR_BOG = createBiome("embur_bog", NetherDescentBiomeBuilder::emburBog);
+	public static final ResourceKey<Biome> SYTHIAN_TORRIDS = createBiome("sythian_torrids", NetherDescentBiomeBuilder::sythianTorrids);
+	public static final ResourceKey<Biome> WAILING_GARTH = createBiome("wailing_garth", NetherDescentBiomeBuilder::wailingGarth);
 
 	@SafeVarargs
 	private static ResourceKey<Biome> createBiome(String id, BiomeFactory biomeFactory, TagKey<Biome>... tags) {
