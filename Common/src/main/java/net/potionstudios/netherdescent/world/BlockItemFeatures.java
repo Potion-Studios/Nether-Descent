bfef646da8117aaf0d137974b63a39f0078cd070
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-10-02 17:51:46 -0400

    Add Logos

--- Common/src/main/java/net/potionstudios/netherdescent/world/BlockItemFeatures.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/BlockItemFeatures.java
@@ -1,6 +1,7 @@
 package net.potionstudios.netherdescent.world;
 
 import net.minecraft.world.level.ItemLike;
+import net.potionstudios.netherdescent.world.item.NetherDescentItems;
 import net.potionstudios.netherdescent.world.level.block.NetherDescentBlocks;
 import net.potionstudios.netherdescent.world.level.block.wood.NetherDescentWoodSet;
 
@@ -9,10 +10,11 @@ import java.util.function.BiConsumer;
 public class BlockItemFeatures {
 
     public static void registerCompostables(BiConsumer<ItemLike, Float> consumer) {
-        compostItems(consumer, 0.5F, NetherDescentBlocks.EMBUR_SPROUTS.get());
-        compostItems(consumer, 0.5F, NetherDescentBlocks.SYTHIAN_SPROUTS.get());
+        compostItems(consumer, 0.3F, NetherDescentItems.CRIMSON_BERRIES.get());
+        compostItems(consumer, 0.5F, NetherDescentBlocks.EMBUR_SPROUTS.get(), NetherDescentBlocks.SYTHIAN_SPROUTS.get());
         compostItems(consumer, 0.6F, NetherDescentBlocks.EMBUR_GEL_VINES.get());
-        compostItems(consumer, 0.65F, NetherDescentBlocks.EMBUR_ROOTS.get(), NetherDescentBlocks.TALL_EMBUR_ROOTS.get(), NetherDescentBlocks.TALL_CRIMSON_ROOTS.get());
+        compostItems(consumer, 0.65F, NetherDescentBlocks.EMBUR_ROOTS.get(), NetherDescentBlocks.TALL_EMBUR_ROOTS.get(), NetherDescentBlocks.TALL_CRIMSON_ROOTS.get(), NetherDescentBlocks.FUNGAL_BULBS.get());
+        compostItems(consumer, 1F, NetherDescentItems.CRIMSON_BERRY_PIE.get());
         NetherDescentWoodSet.woodsets().forEach(set -> compostItems(consumer, 0.65F, set.growerItem().getItem()));
     }
 
