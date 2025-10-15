636dd186a1132513417fd0229e2c8653975b8a47
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-10-14 21:51:27 -0400

    Place Mossy Carpets Correctly

--- Common/src/main/java/net/potionstudios/netherdescent/world/level/levelgen/feature/CarpetPatchFeature.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/level/levelgen/feature/CarpetPatchFeature.java
@@ -5,6 +5,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.potionstudios.netherdescent.world.level.block.custom.MossyCarpetBlock;
 import net.potionstudios.netherdescent.world.level.levelgen.feature.configurations.CarpetPatchFeatureConfiguration;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,13 +16,12 @@ public class CarpetPatchFeature extends Feature<CarpetPatchFeatureConfiguration>
 
 	@Override
 	public boolean place(@NotNull FeaturePlaceContext<CarpetPatchFeatureConfiguration> context) {
-		WorldGenLevel level = context.level();
-		BlockPos origin = context.origin();
-
 		CarpetPatchFeatureConfiguration config = context.config();
 		int radius = config.radius().sample(context.random());
 
 		boolean placed = false;
+		WorldGenLevel level = context.level();
+		BlockPos origin = context.origin();
 
 		for (int x = -radius; x <= radius; x++)
 			for (int z = -radius; z <= radius; z++) {
@@ -29,7 +29,9 @@ public class CarpetPatchFeature extends Feature<CarpetPatchFeatureConfiguration>
 				if (level.isEmptyBlock(pos)) continue;
 				setBlock(level, pos, config.base().defaultBlockState());
 				if (level.isEmptyBlock(pos.above()) && config.carpetChance() <= context.random().nextFloat())
-					setBlock(level, pos.above(), config.carpet().defaultBlockState());
+					if (config.carpet() instanceof MossyCarpetBlock)
+						MossyCarpetBlock.placeAt(level, pos.above(), context.random(), 2);
+					else setBlock(level, pos.above(), config.carpet().defaultBlockState());
 				placed = true;
 			}
 
