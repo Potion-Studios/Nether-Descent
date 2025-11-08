1ef42ebcc9bcd87c19a1de1294f29d1470ce274f
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-11-08 12:19:54 -0500

    Add Tickers

--- Common/src/main/java/net/potionstudios/netherdescent/world/level/block/custom/WailingFungalGillsBlock.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/level/block/custom/WailingFungalGillsBlock.java
@@ -2,26 +2,47 @@ package net.potionstudios.netherdescent.world.level.block.custom;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.potionstudios.netherdescent.world.level.block.entities.NetherDescentBlockEntityType;
 import net.potionstudios.netherdescent.world.level.block.entities.WailingFungalGillsBlockEntity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class WailingFungalGillsBlock extends BaseEntityBlock {
+	private static final MapCodec<WailingFungalGillsBlock> CODEC = simpleCodec(WailingFungalGillsBlock::new);
 	public WailingFungalGillsBlock(Properties properties) {
 		super(properties);
 	}
 
 	@Override
 	protected @NotNull MapCodec<? extends BaseEntityBlock> codec() {
-		return null;
+		return CODEC;
 	}
 
 	@Override
 	public @Nullable BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
 		return new WailingFungalGillsBlockEntity(pos, state);
+	}
+
+	@Override
+	protected @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
+		return RenderShape.MODEL;
+	}
+
+	@Override
+	public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> blockEntityType) {
+		return createTickerHelper(level, blockEntityType, NetherDescentBlockEntityType.WAILING_FUNGAL_GILLS.get());
+	}
+
+	public static <T extends BlockEntity> BlockEntityTicker<T> createTickerHelper(
+			Level level, BlockEntityType<T> serverType, BlockEntityType<? extends WailingFungalGillsBlockEntity> clientType
+	) {
+		return level.isClientSide() ? null : createTickerHelper(serverType, clientType, WailingFungalGillsBlockEntity::serverTick);
 	}
 }
--- Common/src/main/java/net/potionstudios/netherdescent/world/level/block/entities/WailingFungalGillsBlockEntity.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/level/block/entities/WailingFungalGillsBlockEntity.java
@@ -1,11 +1,29 @@
 package net.potionstudios.netherdescent.world.level.block.entities;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 
 public class WailingFungalGillsBlockEntity extends BlockEntity {
 	public WailingFungalGillsBlockEntity(BlockPos pos, BlockState blockState) {
 		super(NetherDescentBlockEntityType.WAILING_FUNGAL_GILLS.get(), pos, blockState);
+	}
+
+	public static void serverTick(Level level, BlockPos pos, BlockState state, WailingFungalGillsBlockEntity blockEntity) {
+		ServerLevel serverLevel = (ServerLevel) level;
+		if (serverLevel.getServer().getTickCount() % 20 == 0) {
+			AABB aabb = new AABB(pos).expandTowards(0.0, 20.0, 0.0);
+			serverLevel.getEntitiesOfClass(Entity.class, aabb).forEach(entity -> {
+				if (entity instanceof ServerPlayer player)
+					if (player.isSpectator()) return;
+
+
+			});
+		}
 	}
 }
