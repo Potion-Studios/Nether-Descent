305bd5a3ff6b449d4fa0227159cc423813529a69
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-11-07 21:21:10 -0500

    Fix Wailing Garth and Crimson Gardens Mob Spawns

--- Common/src/main/java/net/potionstudios/netherdescent/world/level/levelgen/biome/NetherDescentBiomeBuilder.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/level/levelgen/biome/NetherDescentBiomeBuilder.java
@@ -44,11 +44,14 @@ public class NetherDescentBiomeBuilder {
         addVegetal(generationSettings, NetherDescentPlacements.CRIMSON_GARDEN_VEGETATION);
 
         MobSpawnSettings.Builder spawnSettings = new MobSpawnSettings.Builder();
-        addSpawn(spawnSettings, EntityType.HOGLIN, 9, 3, 4);
-        addSpawn(spawnSettings, EntityType.ZOMBIFIED_PIGLIN, 80, 4, 4);
-        addSpawn(spawnSettings, EntityType.MAGMA_CUBE, 100, 2, 5);
-        addSpawn(spawnSettings, EntityType.PIGLIN, 15, 4, 4);
-        addSpawn(spawnSettings, EntityType.STRIDER, 60, 1, 2);
+        addSpawn(spawnSettings, EntityType.SKELETON, 20, 5, 5);
+		addSpawn(spawnSettings, EntityType.GHAST, 50, 4, 4);
+		addSpawn(spawnSettings, EntityType.ENDERMAN, 1, 4, 4);
+		addSpawn(spawnSettings, EntityType.STRIDER, 60, 1, 2);
+		spawnSettings.addMobCharge(EntityType.SKELETON, 0.7, 0.15)
+				.addMobCharge(EntityType.GHAST, 0.7, 0.15)
+				.addMobCharge(EntityType.ENDERMAN, 0.7, 0.15)
+				.addMobCharge(EntityType.STRIDER, 0.7, 0.15);
         return new Biome.BiomeBuilder().hasPrecipitation(false).temperature(2).downfall(0.0F).specialEffects((new BiomeSpecialEffects.Builder()).waterColor(4159204).waterFogColor(329011).fogColor(3343107).skyColor(3343107).ambientParticle(new AmbientParticleSettings(ParticleTypes.CRIMSON_SPORE, 0.01428F)).ambientLoopSound(NetherDescentSounds.AMBIENT_CRIMSON_GARDENS_LOOP.get()).ambientMoodSound(new AmbientMoodSettings(SoundEvents.AMBIENT_CRIMSON_FOREST_MOOD, 6000, 8, 2.0D)).ambientAdditionsSound(new AmbientAdditionsSettings(NetherDescentSounds.AMBIENT_CRIMSON_GARDENS_ADDITIONS.get(), 0.0111D)).backgroundMusic(Musics.createGameMusic(NetherDescentSounds.MUSIC_BIOME_CRIMSON_GARDENS.get())).build()).mobSpawnSettings(spawnSettings.build()).generationSettings(generationSettings.build()).build();
     }
 
@@ -106,13 +109,14 @@ public class NetherDescentBiomeBuilder {
         addVegetal(generationSettings, NetherDescentPlacements.WAILING_GARTH_VEGETATION);
 
         MobSpawnSettings.Builder spawnSettings = new MobSpawnSettings.Builder();
-        addSpawn(spawnSettings, EntityType.GHAST, 50, 4, 4);
-        addSpawn(spawnSettings, EntityType.ZOMBIFIED_PIGLIN, 100, 4, 4);
-        addSpawn(spawnSettings, EntityType.MAGMA_CUBE, 2, 4, 4);
-        addSpawn(spawnSettings, EntityType.ENDERMAN, 1, 4, 4);
-        addSpawn(spawnSettings, EntityType.PIGLIN, 15, 4, 4);
-        addSpawn(spawnSettings, EntityType.STRIDER, 60, 1, 2);
-        addSpawn(spawnSettings, EntityType.HOGLIN, 60, 1, 2);
+	    addSpawn(spawnSettings, EntityType.SKELETON, 20, 5, 5);
+	    addSpawn(spawnSettings, EntityType.GHAST, 50, 4, 4);
+	    addSpawn(spawnSettings, EntityType.ENDERMAN, 1, 4, 4);
+	    addSpawn(spawnSettings, EntityType.STRIDER, 60, 1, 2);
+	    spawnSettings.addMobCharge(EntityType.SKELETON, 0.7, 0.15)
+			    .addMobCharge(EntityType.GHAST, 0.7, 0.15)
+			    .addMobCharge(EntityType.ENDERMAN, 0.7, 0.15)
+			    .addMobCharge(EntityType.STRIDER, 0.7, 0.15);
 
         return new Biome.BiomeBuilder().hasPrecipitation(false).temperature(2).downfall(0.0F).specialEffects((new BiomeSpecialEffects.Builder()).waterColor(4159204).waterFogColor(329011).fogColor(4529794).skyColor(4529794).ambientParticle(new AmbientParticleSettings(ParticleTypes.WARPED_SPORE, 0.01428F)).ambientLoopSound(NetherDescentSounds.AMBIENT_WAILING_GARTH_LOOP.get()).ambientMoodSound(new AmbientMoodSettings(SoundEvents.AMBIENT_SOUL_SAND_VALLEY_MOOD, 6000, 8, 2.0D)).ambientAdditionsSound(new AmbientAdditionsSettings(NetherDescentSounds.AMBIENT_WAILING_GARTH_ADDITIONS.get(), 0.0111D)).backgroundMusic(Musics.createGameMusic(NetherDescentSounds.MUSIC_BIOME_WAILING_GARTH.get())).build()).mobSpawnSettings(spawnSettings.build()).generationSettings(generationSettings.build()).build();
     }
