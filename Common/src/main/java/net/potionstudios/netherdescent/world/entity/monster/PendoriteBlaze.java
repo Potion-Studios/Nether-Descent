4c6f1c90d49ca17f3b22d6734aa7c20a5b9ec028
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-11-20 19:01:37 -0500

    Golem Like Logic for PendoriteBlaze

--- Common/src/main/java/net/potionstudios/netherdescent/world/entity/monster/PendoriteBlaze.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/entity/monster/PendoriteBlaze.java
@@ -4,77 +4,142 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.players.OldUsersConverter;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.TimeUtil;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.OwnableEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.NeutralMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
 import net.minecraft.world.entity.monster.Blaze;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.potionstudios.netherdescent.world.entity.NetherDescentEntityType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
 import java.util.UUID;
 
-public class PendoriteBlaze extends Blaze implements OwnableEntity {
-	protected static final EntityDataAccessor<Optional<UUID>> DATA_OWNERUUID_ID = SynchedEntityData.defineId(PendoriteBlaze.class, EntityDataSerializers.OPTIONAL_UUID);;
+public class PendoriteBlaze extends Blaze implements NeutralMob {
+    protected static final EntityDataAccessor<Byte> DATA_FLAGS_ID = SynchedEntityData.defineId(PendoriteBlaze.class, EntityDataSerializers.BYTE);
+    private static final UniformInt PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(20, 39);
+    private int remainingPersistentAngerTime;
+    @Nullable
+    private UUID persistentAngerTarget;
 
 	public PendoriteBlaze(EntityType<? extends Blaze> entityType, Level level) {
 		super(entityType, level);
 		this.xpReward = 8;
 	}
 
-	public PendoriteBlaze(Level level, UUID owner) {
-		this(NetherDescentEntityType.PENDORITE_BLAZE.get(), level);
-		setOwnerUUID(owner);
-	}
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0, true));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0, 0.0F));
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isAngryAt));
+        this.targetSelector
+                .addGoal(
+                        3, new NearestAttackableTargetGoal<>(this, Mob.class, 5, false, false, livingEntity -> livingEntity instanceof Enemy && !(livingEntity instanceof Creeper))
+                );
+        this.targetSelector.addGoal(4, new ResetUniversalAngerTargetGoal<>(this, false));
+    }
 
-	@Override
-	protected void defineSynchedData(SynchedEntityData.@NotNull Builder builder) {
-		super.defineSynchedData(builder);
-		builder.define(DATA_OWNERUUID_ID, Optional.empty());
-	}
+    @Override
+    protected void defineSynchedData(SynchedEntityData.@NotNull Builder builder) {
+        super.defineSynchedData(builder);
+        builder.define(DATA_FLAGS_ID, (byte) 0);
+    }
 
-	@Override
-	public void addAdditionalSaveData(@NotNull CompoundTag compound) {
-		super.addAdditionalSaveData(compound);
-		if (this.getOwnerUUID() != null)
-			compound.putUUID("Owner", this.getOwnerUUID());
-	}
+    @Override
+    public void addAdditionalSaveData(@NotNull CompoundTag compound) {
+        super.addAdditionalSaveData(compound);
+        compound.putBoolean("PlayerCreated", this.isPlayerCreated());
+        this.addPersistentAngerSaveData(compound);
+    }
 
-	@Override
-	public void readAdditionalSaveData(@NotNull CompoundTag compound) {
-		super.readAdditionalSaveData(compound);
-		UUID uuid;
-		if (compound.hasUUID("Owner")) {
-			uuid = compound.getUUID("Owner");
-		} else {
-			String string = compound.getString("Owner");
-			uuid = OldUsersConverter.convertMobOwnerIfNecessary(this.getServer(), string);
-		}
+    @Override
+    public void readAdditionalSaveData(@NotNull CompoundTag compound) {
+        super.readAdditionalSaveData(compound);
+        this.setPlayerCreated(compound.getBoolean("PlayerCreated"));
+        this.readPersistentAngerSaveData(this.level(), compound);
+    }
 
-		if (uuid != null)
-			setOwnerUUID(uuid);
-	}
-
-	public static AttributeSupplier.@NotNull Builder createAttributes() {
+    public static AttributeSupplier.@NotNull Builder createAttributes() {
 		return Monster.createMonsterAttributes().add(Attributes.ATTACK_DAMAGE, 6.0F).add(Attributes.MOVEMENT_SPEED, 0.23F).add(Attributes.FOLLOW_RANGE, 25.0F);
 	}
 
-	@Override
-	public @Nullable UUID getOwnerUUID() {
-		return this.entityData.get(DATA_OWNERUUID_ID).orElse(null);
-	}
+    @Override
+    public void aiStep() {
+        if (!level().isClientSide())
+            updatePersistentAnger((ServerLevel) level(), true);
+    }
 
-	public void setOwnerUUID(@Nullable UUID uuid) {
-		this.entityData.set(DATA_OWNERUUID_ID, Optional.ofNullable(uuid));
-	}
-
-	@Override
+    @Override
 	protected boolean shouldDropLoot() {
 		return false;
 	}
+
+    @Override
+    public int getRemainingPersistentAngerTime() {
+        return this.remainingPersistentAngerTime;
+    }
+
+    @Override
+    public void setRemainingPersistentAngerTime(int remainingPersistentAngerTime) {
+        this.remainingPersistentAngerTime = remainingPersistentAngerTime;
+    }
+
+    @Override
+    public @Nullable UUID getPersistentAngerTarget() {
+        return this.persistentAngerTarget;
+    }
+
+    @Override
+    public void setPersistentAngerTarget(@Nullable UUID persistentAngerTarget) {
+        this.persistentAngerTarget = persistentAngerTarget;
+    }
+
+    @Override
+    public void startPersistentAngerTimer() {
+        this.setRemainingPersistentAngerTime(PERSISTENT_ANGER_TIME.sample(this.random));
+    }
+
+    public boolean isPlayerCreated() {
+        return (this.entityData.get(DATA_FLAGS_ID) & 1) != 0;
+    }
+
+    public void setPlayerCreated(boolean playerCreated) {
+        byte b = this.entityData.get(DATA_FLAGS_ID);
+        if (playerCreated) {
+            this.entityData.set(DATA_FLAGS_ID, (byte)(b | 1));
+        } else {
+            this.entityData.set(DATA_FLAGS_ID, (byte)(b & -2));
+        }
+    }
+
+    @Override
+    public boolean removeWhenFarAway(double distanceToClosestPlayer) {
+        return false;
+    }
+
+    @Override
+    public boolean canAttackType(@NotNull EntityType<?> entityType) {
+        if (this.isPlayerCreated() && entityType == EntityType.PLAYER) {
+            return false;
+        } else {
+            return entityType != EntityType.CREEPER && super.canAttackType(entityType);
+        }    }
 }
