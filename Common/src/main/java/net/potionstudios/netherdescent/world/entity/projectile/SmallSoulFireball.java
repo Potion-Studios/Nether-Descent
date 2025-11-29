e49bac6aed1621a1786e78f219366678220a9fe1
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-11-29 10:10:46 -0500

    Mixin For Rendering SoulFire on Small Soul Fireball

--- Common/src/main/java/net/potionstudios/netherdescent/world/entity/projectile/SmallSoulFireball.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/entity/projectile/SmallSoulFireball.java
@@ -6,12 +6,14 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Fireball;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.potionstudios.netherdescent.world.entity.NetherDescentEntityType;
+import net.potionstudios.netherdescent.world.item.NetherDescentItems;
 import org.jetbrains.annotations.NotNull;
 
 public class SmallSoulFireball extends Fireball {
@@ -48,4 +50,9 @@ public class SmallSoulFireball extends Fireball {
         if (!level().isClientSide())
             discard();
     }
+
+	@Override
+	public @NotNull ItemStack getItem() {
+		return NetherDescentItems.SOUL_FIRE_CHARGE.get().getDefaultInstance();
+	}
 }
