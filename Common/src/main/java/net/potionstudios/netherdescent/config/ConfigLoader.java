4a66333dc51310def9fb6c5a48d7ff3ad50997bb
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-04-26 19:32:07 -0400

    Regen config on Fail instead of Crash

--- Common/src/main/java/net/potionstudios/netherdescent/config/ConfigLoader.java
+++ Common/src/main/java/net/potionstudios/netherdescent/config/ConfigLoader.java
@@ -3,6 +3,7 @@ package net.potionstudios.netherdescent.config;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import net.potionstudios.netherdescent.NetherDescent;
 import net.potionstudios.netherdescent.PlatformHandler;
 import org.jetbrains.annotations.NotNull;
 
@@ -25,29 +26,40 @@ public class ConfigLoader {
 	 * @return The config file.
 	 */
 	public static <T> T loadConfig(@NotNull Class<T> clazz, String name) {
+		Path configPath = PlatformHandler.PLATFORM_HANDLER.configPath().resolve(name + ".json");
+
 		try {
-			Path configPath = PlatformHandler.PLATFORM_HANDLER.configPath().resolve(name + ".json");
+			if (Files.notExists(configPath.getParent()))
+				Files.createDirectories(configPath.getParent());
+
 			T value = clazz.getConstructor().newInstance();
 
-			if (Files.notExists(configPath)) Files.createDirectories(configPath.getParent());
-			else if (Files.exists(configPath)) value = GSON.fromJson(Files.newBufferedReader(configPath), clazz);
+			if (Files.exists(configPath))
+				value = GSON.fromJson(Files.newBufferedReader(configPath), clazz);
 
 			Files.writeString(configPath, GSON.toJson(value));
 			return value;
 		} catch (Exception e) {
-			throw new RuntimeException("Failed to load config.", e);
+			try {
+				T value = clazz.getConstructor().newInstance();
+				Files.writeString(configPath, GSON.toJson(value));
+				NetherDescent.LOGGER.warn("Failed to load config '{}'. It has been regenerated and all settings have been reset to their default values.", name, e);
+				return value;
+			} catch (Exception regenerationException) {
+				throw new RuntimeException("Failed to regenerate config.", regenerationException);
+			}
 		}
 	}
 
-    public static <T> void saveConfig(@NotNull T config, String name) {
-        try {
-            Path path = PlatformHandler.PLATFORM_HANDLER.configPath().resolve(name + ".json");
-            if (Files.notExists(path.getParent()))
-                Files.createDirectories(path.getParent());
+	public static <T> void saveConfig(@NotNull T config, String name) {
+		try {
+			Path path = PlatformHandler.PLATFORM_HANDLER.configPath().resolve(name + ".json");
+			if (Files.notExists(path.getParent()))
+				Files.createDirectories(path.getParent());
 
-            Files.writeString(path, GSON.toJson(config));
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to save biome config", e);
-        }
-    }
+			Files.writeString(path, GSON.toJson(config));
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to save biome config", e);
+		}
+	}
 }
