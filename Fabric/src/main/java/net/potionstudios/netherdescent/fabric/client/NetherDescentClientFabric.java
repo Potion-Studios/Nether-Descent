9e56cd661574da91e0c7f741e923fe37582c7021
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-04-27 19:47:57 -0400

    Fix Pendorite Torch RenderType on Fabric

--- Fabric/src/main/java/net/potionstudios/netherdescent/fabric/client/NetherDescentClientFabric.java
+++ Fabric/src/main/java/net/potionstudios/netherdescent/fabric/client/NetherDescentClientFabric.java
@@ -41,7 +41,8 @@ public class NetherDescentClientFabric implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.translucent(), NetherDescentBlocks.EMBUR_GEL_BLOCK.get(), NetherDescentBlocks.EMBUR.door(), NetherDescentBlocks.EMBUR.trapdoor(),
 		        NetherDescentBlocks.EMBUR_GEL_VINES.get(), NetherDescentBlocks.EMBUR_GEL_VINES_PLANT.get());
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(), NetherDescentBlocks.CRIMSON_CARPET.get(), NetherDescentBlocks.SYTHIAN_SHOOT.get(), NetherDescentBlocks.SYTHIAN_STALK.getBlock(),
-				NetherDescentBlocks.PENDORITE_CAMPFIRE.get(), NetherDescentBlocks.WAILING_BULB_BLOSSOM.get(), NetherDescentBlocks.ARISIAN_MOSS_BLOCK.get(), NetherDescentBlocks.THORN_SPROUT.get());
+				NetherDescentBlocks.PENDORITE_CAMPFIRE.get(), NetherDescentBlocks.WAILING_BULB_BLOSSOM.get(), NetherDescentBlocks.ARISIAN_MOSS_BLOCK.get(), NetherDescentBlocks.THORN_SPROUT.get(),
+				NetherDescentBlocks.PENDORITE_WALL_TORCH.get(), NetherDescentBlocks.PENDORITE_TORCH.get());
 	}
 
 	private void renderTypeBlock(Block block) {
