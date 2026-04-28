41398ded6e7b7d29e3f6fb5be95c8ab2369c79b4
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-04-28 09:40:03 -0400

    Fix Config for Biolith

--- Common/src/main/java/net/potionstudios/netherdescent/world/level/levelgen/biome/SurfaceRuleRegisterBiolith.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/level/levelgen/biome/SurfaceRuleRegisterBiolith.java
@@ -4,12 +4,14 @@ import com.terraformersmc.biolith.api.biome.BiomePlacement;
 import com.terraformersmc.biolith.api.surface.SurfaceGeneration;
 import net.minecraft.world.level.biome.Climate;
 import net.potionstudios.netherdescent.NetherDescent;
+import net.potionstudios.netherdescent.config.configs.WorldGenerationConfig;
 
 public class SurfaceRuleRegisterBiolith {
 
     public static void registerSurfaceRules() {
         NetherDescentBiomes.BIOME_FACTORIES.forEach((biomeKey, biomeFactory) -> {
-            BiomePlacement.addNether(biomeKey, Climate.parameters(1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.2F, 0.0F));
+            if (WorldGenerationConfig.get().isEnabled(biomeKey))
+                BiomePlacement.addNether(biomeKey, Climate.parameters(1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.2F, 0.0F));
         });
         SurfaceGeneration.addNetherSurfaceRules(NetherDescent.id("rules/nether"), NetherDescentSurfaceRules.makeRules());
     }
