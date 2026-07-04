93d3ec29a39f25fe2175e584d2774ba6690e9d9f
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-07-04 13:07:26 -0400

    ServerEventsHandler

--- Common/src/main/java/net/potionstudios/netherdescent/event/ServerEventsHandler.java
+++ Common/src/main/java/net/potionstudios/netherdescent/event/ServerEventsHandler.java
@@ -4,26 +4,15 @@ import com.terraformersmc.biolith.impl.Biolith;
 import dev.worldgen.lithostitched.Lithostitched;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.permissions.Permissions;
 import net.minecraft.util.CommonColors;
-import net.minecraft.world.level.GameType;
-import net.minecraft.world.level.Level;
 import net.potionstudios.netherdescent.PlatformHandler;
-import net.potionstudios.netherdescent.config.configs.DevConfig;
 import terrablender.core.TerraBlender;
 
 public class ServerEventsHandler {
 	public static void onPlayerJoin(ServerPlayer serverPlayer) {
 		if (!PlatformHandler.PLATFORM_HANDLER.isModLoaded(TerraBlender.MOD_ID) && !PlatformHandler.PLATFORM_HANDLER.isModLoaded(Biolith.MOD_ID) && !PlatformHandler.PLATFORM_HANDLER.isModLoaded(Lithostitched.MOD_ID))
-			if (serverPlayer.getServer().isSingleplayer() || serverPlayer.hasPermissions(2))
+			if (serverPlayer.level().getServer().isSingleplayer() || serverPlayer.permissions().hasPermission(Permissions.COMMANDS_ADMIN))
 				serverPlayer.sendSystemMessage(Component.literal("TerraBlender, Biolith, or Lithostitched are not loaded, Nether Descent's biomes will not be added to the Nether!").withColor(CommonColors.RED));
-
-		if (PlatformHandler.PLATFORM_HANDLER.isDevEnvironment())
-			if (serverPlayer.level().dimension() != Level.NETHER) {
-				DevConfig devConfig = DevConfig.getInstance(true);
-				if (devConfig.startInNether()) {
-					serverPlayer.setGameMode(GameType.SPECTATOR);
-					serverPlayer.teleportTo(serverPlayer.getServer().getLevel(Level.NETHER), serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ(), serverPlayer.getYRot(), serverPlayer.getXRot());
-				}
-			}
 	}
 }
