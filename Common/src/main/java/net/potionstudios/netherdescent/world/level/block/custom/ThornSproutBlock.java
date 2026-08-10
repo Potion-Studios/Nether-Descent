e7073ee77b080613bf6a1cf83a6ecff33744d963
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-08-10 13:05:14 -0400

    Extension Logic
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/netherdescent/world/level/block/custom/ThornSproutBlock.java
+++ Common/src/main/java/net/potionstudios/netherdescent/world/level/block/custom/ThornSproutBlock.java
@@ -2,8 +2,12 @@ package net.potionstudios.netherdescent.world.level.block.custom;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
@@ -11,6 +15,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
@@ -25,6 +30,7 @@ public class ThornSproutBlock extends HorizontalDirectionalBlock {
 	public static final EnumProperty<SegmentType> SEGMENT = EnumProperty.create("segment", SegmentType.class);
 	public static final BooleanProperty FLOWERING = BooleanProperty.create("flowering");
 	public static final IntegerProperty SIZE = IntegerProperty.create("size", 0, 17);
+	public static final BooleanProperty PERSISTENT = BlockStateProperties.PERSISTENT;
 
 	public ThornSproutBlock(Properties properties) {
 		super(properties);
@@ -36,10 +42,53 @@ public class ThornSproutBlock extends HorizontalDirectionalBlock {
 		if (context.getClickedFace().getAxis().isHorizontal()) {
 			BlockPos attachPos = context.getClickedPos().relative(context.getClickedFace().getOpposite());
 			BlockState attachState = context.getLevel().getBlockState(attachPos);
-			if (attachState.isFaceSturdy(context.getLevel(), attachPos, context.getClickedFace()) || (attachState.is(this) && attachState.getValue(SEGMENT).equals(SegmentType.END) && attachState.getValue(SIZE) != 17))
+			if (attachState.isFaceSturdy(context.getLevel(), attachPos, context.getClickedFace()) || (attachState.is(this) && attachState.getValue(SEGMENT).equals(SegmentType.END) && attachState.getValue(SIZE) != 17)){
+				if (attachState.is(this)) {
+					context.getLevel().setBlockAndUpdate(attachPos, attachState.setValue(SEGMENT, SegmentType.MIDDLE));
+				}
 				return defaultBlockState().setValue(FACING, context.getClickedFace()).setValue(SIZE, 0);
+			}
+
 		}
 		return null;
+	}
+
+	@Override
+	protected void tick(@NotNull BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
+		super.tick(state, level, pos, random);
+	}
+
+	@Override
+	public void stepOn(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull Entity entity) {
+		super.stepOn(level, pos, state, entity);
+		if (entity instanceof LivingEntity) {
+			int size = state.getValue(SIZE);
+			if (size < 17 && state.getValue(SEGMENT).equals(SegmentType.END)) {
+				extend(level, pos, state, level.getRandom());
+			}
+		}
+	}
+
+	private void extend(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull RandomSource randomSource) {
+		int size = state.getValue(SIZE);
+		Direction direction = state.getValue(FACING);
+		for (int i = 1; i <= 3; i++) {
+			int spotSize = size + i;
+			BlockPos spotPos = pos.relative(direction, i);
+			if (spotSize > 17 || !level.isEmptyBlock(spotPos))
+				return;
+			else {
+				level.setBlockAndUpdate(spotPos.relative(direction.getOpposite(), 1), state.setValue(SEGMENT, state.getValue(SIZE) == 0 ? SegmentType.BASE : SegmentType.MIDDLE).setValue(FLOWERING, randomSource.nextBoolean()));
+				level.scheduleTick(spotPos.relative(direction.getOpposite(), 1), this, 301);
+				level.setBlockAndUpdate(spotPos, state.setValue(SEGMENT, SegmentType.END));
+				level.scheduleTick(spotPos, this, 300);
+			}
+		}
+	}
+
+	@Override
+	protected void neighborChanged(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Block neighborBlock, @NotNull BlockPos neighborPos, boolean movedByPiston) {
+		super.neighborChanged(state, level, pos, neighborBlock, neighborPos, movedByPiston);
 	}
 
 	@Override
@@ -65,7 +114,7 @@ public class ThornSproutBlock extends HorizontalDirectionalBlock {
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder) {
-		super.createBlockStateDefinition(builder.add(SEGMENT, FLOWERING, SIZE, FACING));
+		super.createBlockStateDefinition(builder.add(SEGMENT, FLOWERING, SIZE, FACING, PERSISTENT));
 	}
 
 	public enum SegmentType implements StringRepresentable {
